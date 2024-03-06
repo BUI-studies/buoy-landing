38924e361b42c755ce4a9050ad66e33cc5939b8c
Roman Ohiienko <ohiienko.r@gmail.com>
2024-03-06 14:59:29 +0200

    Added a few lines of tranclstions. Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import { useTranslation } from "react-i18next"
-
-import "./App.css"
+import { useTranslation } from 'react-i18next'
 
 function App() {
-  const { t, i18n } = useTranslation()
+	const { t, i18n } = useTranslation()
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>{t("siteTitle")}</p>
-      </div>
-      <p>
-        lang:
-        <button
-          onClick={() => i18n.changeLanguage("ad")}
-          className={i18n.language === "ad" ? "btn-active" : ""}
-        >
-          adults
-        </button>
-        <button
-          onClick={() => i18n.changeLanguage("ch")}
-          className={i18n.language === "ch" ? "btn-active" : ""}
-        >
-          children
-        </button>
-      </p>
-    </>
-  )
+	return (
+		<>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<p>{t('heroTitle')}</p>
+			</div>
+			<p>
+				lang:
+				<button
+					onClick={() => i18n.changeLanguage('ad')}
+					className={i18n.language === 'ad' ? 'btn-active' : ''}
+				>
+					adults
+				</button>
+				<button
+					onClick={() => i18n.changeLanguage('ch')}
+					className={i18n.language === 'ch' ? 'btn-active' : ''}
+				>
+					children
+				</button>
+			</p>
+		</>
+	)
 }
 
 export default App
