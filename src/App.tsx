31bf4684fc92394300480941a9dd9913599aa468
Roman Ohiienko <ohiienko.r@gmail.com>
2024-04-23 23:29:30 +0300

    Complete markup and desctop styling of study results

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from './sections/Hero'
 import StudyFormat from './sections/StudyFormat'
+import StudyResults from './sections/StudyResults'
 
 function App() {
 	return (
 		<>
 			<Hero />
 			<StudyFormat />
+			<StudyResults />
 		</>
 	)
 }
